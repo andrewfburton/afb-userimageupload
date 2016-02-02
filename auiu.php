ad434b54d7fdeb1f8e771242ff104d8e8c2aef1e
andrewfburton <andrewfburton@gmail.com>
2016-02-02 16:15:50 +0200

    More Plugin Update testing

--- auiu.php
+++ auiu.php
@@ -4,7 +4,7 @@ Plugin Name: AFB User Image Upload
 Plugin URI: http://www.ifihadthecash.org
 Description: Let your web site visitors upload images, directly into your image gallery
 Author: Tareq Hasan & AFB
-Version: 1.0.1
+Version: 1.0.0
 Author URI: http://www.ifihadthecash.org
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -32,7 +32,6 @@ class auiu_Main {
         add_action( 'admin_init', array($this, 'block_admin_access') );
         add_action( 'init', array($this, 'load_textdomain') );
         add_action( 'wp_enqueue_scripts', array($this, 'enqueue_scripts') );
-		add_action( 'init', array($this, 'afb_github_plugin_updater_init') );
     }
     /**
      * Create tables on plugin activation
@@ -57,6 +56,25 @@ class auiu_Main {
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 
         $wpdb->query( $sql_custom );
+		
+		include_once 'updater.php';
+		define( 'WP_GITHUB_FORCE_UPDATE', true );
+		if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+			$config = array(
+				'slug' => plugin_basename( __FILE__ ),
+				'proper_folder_name' => 'github-updater',
+				'api_url' => 'https://api.github.com/repos/andrewfburton/afb-userimageupload',
+				'raw_url' => 'https://raw.github.com/andrewfburton/afb-userimageupload/master',
+				'github_url' => 'https://github.com/andrewfburton/afb-userimageupload',
+				'zip_url' => 'https://github.com/andrewfburton/afb-userimageupload/archive/master.zip',
+				'sslverify' => true,
+				'requires' => '3.0',
+				'tested' => '3.3',
+				'readme' => 'README.md',
+				'access_token' => '',
+			);
+			new WP_GitHub_Updater( $config );
+		}
     }
 
     function uninstall() {
@@ -145,26 +163,6 @@ class auiu_Main {
             load_textdomain( 'auiu', $mofile );
         }
     }
-	function afb_github_plugin_updater_init() {
-		include_once 'updater.php';
-		define( 'WP_GITHUB_FORCE_UPDATE', true );
-		if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-			$config = array(
-				'slug' => plugin_basename( __FILE__ ),
-				'proper_folder_name' => 'github-updater',
-				'api_url' => 'https://api.github.com/repos/andrewfburton/afb-userimageupload',
-				'raw_url' => 'https://raw.github.com/andrewfburton/afb-userimageupload/master',
-				'github_url' => 'https://github.com/andrewfburton/afb-userimageupload',
-				'zip_url' => 'https://github.com/andrewfburton/afb-userimageupload/archive/master.zip',
-				'sslverify' => true,
-				'requires' => '3.0',
-				'tested' => '3.3',
-				'readme' => 'README.md',
-				'access_token' => '',
-			);
-			new WP_GitHub_Updater( $config );
-		}
-	}	
     /**
      * The main logging function
      *
