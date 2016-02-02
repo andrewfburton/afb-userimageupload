1454aabe0b11ccca9e8b747d40fdcc191e8442ce
andrewfburton <andrewfburton@gmail.com>
2016-02-02 17:51:19 +0200

    Change in updater

--- updater.php
+++ updater.php
@@ -300,7 +300,8 @@ class WP_GitHub_Updater {
 				$github_data = json_decode( $github_data['body'] );
 
 				// refresh every 6 hours
-				set_site_transient( md5($this->config['slug']).'_github_data', $github_data, 60*60*6 );
+				//set_site_transient( md5($this->config['slug']).'_github_data', $github_data, 60*60*6 );
+				set_site_transient('update_plugins', null);
 			}
 
 			// Store the data in this class instance for future calls
